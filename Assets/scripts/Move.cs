9e7a005477bd58ea6e01a4366538ff34a78d1d16
Mister Lobos <lukaslobossa@gmail.com>
2024-08-20 16:04:23 -0400

    Correccion de Scripts

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-
-    new Vector2 Input;
+    
+   Vector2 Input;
 
     // Start is called before the first frame update
     void Start()
